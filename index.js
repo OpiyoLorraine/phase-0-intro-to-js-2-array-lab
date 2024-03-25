60ff206823cc8d91591b1e7fef94415dddcbc136
Lorraine Opiyo <achienglorraine8@gmail.com>
2024-03-25 22:21:29 +0300

    phase-0-intro-to-js-2-array-lab

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name) {
+    const newCats = cats.slice(); // Create a copy of the first cats array
+    newCats.push(name);
+    return newCats;
+}
+
+function prependCat(name) {
+    const newCats = cats.slice(); // Create a copy of the first cats array
+    newCats.unshift(name);
+    return newCats;
+}
+
+function removeLastCat() {
+    const newCats = cats.slice(); // Create a copy of the first cats array
+    newCats.pop();
+    return newCats;
+}
+
+function removeFirstCat() {
+    const newCats = cats.slice(); // Create a copy of the first cats array
+    newCats.shift();
+    return newCats;
+}
